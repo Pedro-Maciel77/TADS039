db8d14af2a5180638c67113446f6157056e10faf
pedro-maciel77 <ph385574@gmail.com>
2025-05-28 13:22:38 +0000

    Resolve conflitos de merge

---
+++ CLT/AulaBootstrap/js/HAHAHAHA.js
@@ -0,0 +1,5 @@
+function hackeando() {
+    let nomeComp = document.getElementById("nomeComp").innerText = 'Pedro Henrique';
+    document.getElementById("nomeComp").innerHTML = "HACKED";
+    document.title = 'Curriculo -  Pedro Henrique';
+}
